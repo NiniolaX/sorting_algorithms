b40516368f8576cc45ab436f37515b91b081f05f
Dev Manuels <Emmanuel.umeile@gmail.com>
2023-09-25 23:38:27 +0100

    fix betty errors and edit readme

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -10,19 +10,19 @@ int *merge_sort_helper(int *array, size_t size);
  */
 void merge_sort(int *array, size_t size)
 {
-        size_t i;
-        int *sorted_array;
+	size_t i;
+	int *sorted_array;
 
-        if (array == NULL || size < 2)
-                return;
+	if (array == NULL || size < 2)
+		return;
 
-        sorted_array = merge_sort_helper(array, size);
+	sorted_array = merge_sort_helper(array, size);
 
-        /* Copy values in sorted array to original array */
-        for (i = 0; i < size; i++)
-                array[i] = sorted_array[i];
+	/* Copy values in sorted array to original array */
+	for (i = 0; i < size; i++)
+		array[i] = sorted_array[i];
 
-        free(sorted_array);
+	free(sorted_array);
 }
 
 
@@ -34,57 +34,57 @@ void merge_sort(int *array, size_t size)
  */
 int *merge_sort_helper(int *array, size_t size)
 {
-        size_t mid = size / 2;
-        int *left_half = array, *right_half = array + mid;
-        int *new;
-        size_t i, j, k;
+	size_t mid = size / 2;
+	int *left_half = array, *right_half = array + mid;
+	int *new;
+	size_t i, j, k;
 
-        if (size <= 1)
-                return (array);
+	if (size <= 1)
+		return (array);
 
 
-        /* Allocate memory for new array */
-        new = malloc(sizeof(int) * size);
-        if (new == NULL)
-                return (NULL);
+	/* Allocate memory for new array */
+	new = malloc(sizeof(int) * size);
+	if (new == NULL)
+		return (NULL);
 
-        /* Sort left half */
-        left_half = merge_sort_helper(left_half, mid);
+	/* Sort left half */
+	left_half = merge_sort_helper(left_half, mid);
 
-        /* Sort right half */
-        right_half = merge_sort_helper(right_half, size - mid);
+	/* Sort right half */
+	right_half = merge_sort_helper(right_half, size - mid);
 
-        /* Merge sorted halves */
-        printf("Merging...\n");
-        i = j = k = 0;
-        while (i < mid && j < size - mid)
-        {
-                if (left_half[i] < right_half[j])
-                {
-                        new[k] = left_half[i];
-                        i++;
-                }
-                else
-                {
-                        new[k] = right_half[j];
-                        j++;
-                }
-                k++;
-        }
-        printf("[left]: ");
-        print_array(left_half, mid);
+	/* Merge sorted halves */
+	printf("Merging...\n");
+	i = j = k = 0;
+	while (i < mid && j < size - mid)
+	{
+		if (left_half[i] < right_half[j])
+		{
+			new[k] = left_half[i];
+			i++;
+		}
+		else
+		{
+			new[k] = right_half[j];
+			j++;
+		}
+		k++;
+	}
+	printf("[left]: ");
+	print_array(left_half, mid);
 
-        printf("[right]: ");
-        print_array(right_half, size - mid);
+	printf("[right]: ");
+	print_array(right_half, size - mid);
 
-        /* Copy remaining elements from each half, if any */
-        while (i < mid)
-                new[k++] = left_half[i++];
-        while (j < size - mid)
-                new[k++] = right_half[j++];
+	/* Copy remaining elements from each half, if any */
+	while (i < mid)
+		new[k++] = left_half[i++];
+	while (j < size - mid)
+		new[k++] = right_half[j++];
 
-        printf("[Done]: ");
-        print_array(new, size);
+	printf("[Done]: ");
+	print_array(new, size);
 
-        return (new);
+	return (new);
 }
